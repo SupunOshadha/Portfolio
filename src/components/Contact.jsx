89e12ea968695cd4b49c0317e9481ebf685313de
SupunOshadha <supunoshadha23@gmail.com>
2024-10-26 16:20:37 +0530

    add-7

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,44 +10,43 @@ import { FaGithub } from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <section className=' bg-zinc-700  text-white -mt-10'>
-    <div id='Contact' className='min-w-fit h-1/2'>
-    
-      <div className=' '>        
-        <h1 className='text-center text-4xl pt-14 '>Contact Me</h1>       
-        <hr className='border-orange-400 border-b-4 w-2/5 '></hr>     
+    <section className=' bg-zinc-700  text-white '>
+    <div id='Contact' className='min-w-fit h-1/2 '>
+      <div className='flex flex-row justify-between'>        
+        <hr className='border-orange-400 border-b-2 w-2/5 md:border-orange-400 md:border-b-4'></hr>
+        <h1 className='text-center text-4xl -mt-5'>Contact Me</h1>           
         <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
       </div>
-        <div className='text-center mix-blend-screen grid grid-rows-2 grid-cols-8 justify-center mt-10 gap-4  text-orange-300   text-xl'>
-          <div className='hover:bg-orange-500 hover:text-black col-start-3 col-end-5 bg-zinc-800 rounded-md shadow-xl
+        <div className='text-center mix-blend-screen md:grid grid-rows-2 md:grid-cols-8 justify-center mt-10 gap-4  text-orange-300   text-xl'>
+          <div className='hover:bg-orange-500 hover:text-black col-start-1 col-end-4 md:col-start-3 md:col-end-5 bg-zinc-800 rounded-md shadow-xl
           outline-offset-4 flex flex-row justify-center items-center gap-2 '>
           <a className='flex items-center gap-2' target='_blank' href=''><FaEnvelope />supunoshadha23@gmail.com</a></div>
-          <div className=' hover:bg-orange-500 hover:text-black col-end-7 col-span-2  bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2 '>
+          <div className=' hover:bg-orange-500 hover:text-black md:col-end-7 md:col-span-2  bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2 col-end-8 col-span-3'>
           <a className='flex items-center gap-2' ><FaPhone />(+94) 071-040-6742</a></div>
-          <div className='hover:bg-orange-500 hover:text-black col-start-4 col-end-6   bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2'><a className='flex items-center gap-2'>
+          <div className='hover:bg-orange-500 hover:text-black md:col-start-4 md:col-end-6   bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2'><a className='flex items-center gap-2 col-start-3 col-end-6'>
           <FaMapMarker />Wattala,Sri lanka
           </a></div>
         </div>
-        <div className=' '>        
-        <h1 className='text-center text-4xl pt-14 '></h1>       
-        <hr className='border-orange-400 border-b-4 w-2/5 '></hr>     
-        <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
-      </div>
-      <section className='bg-zinc-800'>
-      <div className=' flex flex-row gap-3 justify-center z-20 mt-2'>
+        <div className=' flex flex-row justify-between mt-10'>              
+        <hr className='border-orange-400 border-b-4 w-2/5 '></hr>    
+        <div className=' flex flex-row gap-3 justify-center z-20 -mt-3 '>
         <a target='_blank' href='https://github.com/SupunOshadha'>
-        <FaGithub className='mt-2 size-8 text-white' />
+        <FaGithub className='size-8 text-white' />
         </a>
         <a target='_blank' href='https://www.facebook.com/profile.php?id=100010352081055&mibextid=ZbWKwL'>
-        <FaFacebook className='size-8 text-white mt-2' />
+        <FaFacebook className='size-8 text-white' />
         </a>
         <a target='_blank' href=' https://wa.me/qr/37WLE5PID5WPA1'>
-        <FaWhatsapp className='size-8 text-white mt-2'/>
+        <FaWhatsapp className='size-8 text-white '/>
         </a>
         <a target='_blank' href='https://www.linkedin.com/in/supunoshadha?'>
-        <FaLinkedinIn className='size-8 text-white mt-2'/> 
+        <FaLinkedinIn className='size-8 text-white '/> 
         </a>
-    </div>
+    </div> 
+        <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
+      </div>
+      <section >
+      
       <footer className='mt-4'>
         <h5 className='text-xl text-center'>All right reserved @Nexnova_Supun </h5>
       </footer>    
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <section className=' bg-zinc-700  text-white -mt-10'>
         <div id='Portfolio'>
-          <h1 className='text-center text-4xl pt-14'>Portfolio</h1>
+          <h1 className='text-center text-4xl pt-14 mb-10'>Portfolio</h1>
             <div className='grid-cols-1'>
 
             </div>
