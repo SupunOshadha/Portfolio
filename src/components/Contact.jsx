805ca39a89684ddefd70faf594e8ae2193f1c537
SupunOshadha <supunoshadha23@gmail.com>
2024-10-26 14:37:52 +0530

    add-6

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,57 @@
 import React from 'react'
+import { FaEnvelope } from 'react-icons/fa'
+import { FaPhone } from 'react-icons/fa'
+import { FaMapMarker } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
+import { FaWhatsapp } from 'react-icons/fa'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { FaEllipsisVertical } from 'react-icons/fa6'
+import { FaGithub } from 'react-icons/fa'
 
 const Contact = () => {
   return (
     <section className=' bg-zinc-700  text-white -mt-10'>
-    <div id='Contact'>
-      <h1 className='text-center text-4xl pt-14'>Contact Me</h1>
-      <hr className='border-orange-400'></hr>
-        <div className='flex-1'>
-
+    <div id='Contact' className='min-w-fit h-1/2'>
+    
+      <div className=' '>        
+        <h1 className='text-center text-4xl pt-14 '>Contact Me</h1>       
+        <hr className='border-orange-400 border-b-4 w-2/5 '></hr>     
+        <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
+      </div>
+        <div className='text-center mix-blend-screen grid grid-rows-2 grid-cols-8 justify-center mt-10 gap-4  text-orange-300   text-xl'>
+          <div className='hover:bg-orange-500 hover:text-black col-start-3 col-end-5 bg-zinc-800 rounded-md shadow-xl
+          outline-offset-4 flex flex-row justify-center items-center gap-2 '>
+          <a className='flex items-center gap-2' target='_blank' href=''><FaEnvelope />supunoshadha23@gmail.com</a></div>
+          <div className=' hover:bg-orange-500 hover:text-black col-end-7 col-span-2  bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2 '>
+          <a className='flex items-center gap-2' ><FaPhone />(+94) 071-040-6742</a></div>
+          <div className='hover:bg-orange-500 hover:text-black col-start-4 col-end-6   bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2'><a className='flex items-center gap-2'>
+          <FaMapMarker />Wattala,Sri lanka
+          </a></div>
         </div>
-
+        <div className=' '>        
+        <h1 className='text-center text-4xl pt-14 '></h1>       
+        <hr className='border-orange-400 border-b-4 w-2/5 '></hr>     
+        <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
+      </div>
+      <section className='bg-zinc-800'>
+      <div className=' flex flex-row gap-3 justify-center z-20 mt-2'>
+        <a target='_blank' href='https://github.com/SupunOshadha'>
+        <FaGithub className='mt-2 size-8 text-white' />
+        </a>
+        <a target='_blank' href='https://www.facebook.com/profile.php?id=100010352081055&mibextid=ZbWKwL'>
+        <FaFacebook className='size-8 text-white mt-2' />
+        </a>
+        <a target='_blank' href=' https://wa.me/qr/37WLE5PID5WPA1'>
+        <FaWhatsapp className='size-8 text-white mt-2'/>
+        </a>
+        <a target='_blank' href='https://www.linkedin.com/in/supunoshadha?'>
+        <FaLinkedinIn className='size-8 text-white mt-2'/> 
+        </a>
+    </div>
+      <footer className='mt-4'>
+        <h5 className='text-xl text-center'>All right reserved @Nexnova_Supun </h5>
+      </footer>    
+      </section>
     </div> 
 </section>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import { FaFacebook } from 'react-icons/fa'
 import { FaWhatsapp } from 'react-icons/fa'
 import { FaLinkedinIn } from 'react-icons/fa'
 import { FaEllipsisVertical } from 'react-icons/fa6'
+import { FaGithub } from 'react-icons/fa'
 const Hero = () => {
   return (
     <section className='w-full h-auto bg-zinc-800 p-4 box-border drop-shadow-2xl  siz-48 md:size-full flow-root'>  
@@ -12,30 +13,24 @@ const Hero = () => {
           <FaEllipsisVertical className='size-8 text-orange-500 '/>
           <FaEllipsisVertical className='size-8 text-orange-500 '/>
           <FaEllipsisVertical className='size-8 text-orange-500 mb-6'/>
-          <a target='_blank' href=''>
-        <FaFacebook className='size-8 text-white' />
+          <a target='_blank' href='https://github.com/SupunOshadha'>
+        <FaGithub className='size-8 text-white' />
         </a>
-        <a target='_blank' href=''>
+        <a target='_blank' href='https://www.facebook.com/profile.php?id=100010352081055&mibextid=ZbWKwL'>
+        <FaFacebook className='size-8 text-white mt-2' />
+        </a>
+        <a target='_blank' href=' https://wa.me/qr/37WLE5PID5WPA1'>
         <FaWhatsapp className='size-8 text-white mt-2'/>
         </a>
-        <a target='_blank' href=''>
+        <a target='_blank' href='https://www.linkedin.com/in/supunoshadha?'>
         <FaLinkedinIn className='size-8 text-white mt-2'/> 
         </a>
     </div>
-    <div className=' absolute'>
-    <div className='w-8 h-8 rounded-full bg-orange-500 animate-bounce bottom-2'>
-    </div>
-    <div className='w-8 h-8 rounded-full bg-orange-500 mt-2'>
-    </div>
-    <div className='w-8 h-8 rounded-full bg-orange-500 mt-2'>
-    </div>
-    </div>
-    
     <div className='z-20'>  
       <h1 className='text-white text-4xl'>I'm <span className='text-orange-500 text-6xl'>Supun Oshadha</span></h1>
       <h1 className='text-white text-4xl mt-2'>Full stack developer</h1>
       <h1 className='text-white text-4xl mt-2'><span className='text-2xl'>founder of</span> NexNova</h1>
-      <button className='text-orange-500 border-2 rounded-lg border-orange-500 p-2 text-xl mt-8'>Contact Me</button>
+      <button onClick={()=> (href="#Contact")} className='text-orange-500 border-2 rounded-lg border-orange-500 p-2 text-xl mt-8'>Contact Me</button>
       </div>
       <div className='relative '>
       <img src={Person} className='grayscale  max-h-96 rounded w-full min-h-96 min-w-72 md:max-h-screen z-10 opacity-60  mix-blend-lighten  hover:filter-none hover:animate-bounce
