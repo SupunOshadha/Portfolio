196a71c8e066877acd29cb41d0de6e866d6697b3
SupunOshadha <supunoshadha23@gmail.com>
2024-10-26 16:49:49 +0530

    add-11

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,14 +17,14 @@ const Contact = () => {
         <h1 className='text-center text-xl md:text-4xl -mt-5'>Contact Me</h1>           
         <hr className='w-1/5 border-b-2 border-orange-400 md:border-b-4 md:w-2/5 float-right '></hr>
       </div>
-        <div className='text-center mix-blend-screen md:grid grid-rows-2 md:grid-cols-8 justify-center mt-10 gap-4  text-orange-300   text-xl'>
-          <div className='hover:bg-orange-500 hover:text-black col-start-1 col-end-4 md:col-start-3 md:col-end-5 bg-zinc-800 rounded-md shadow-xl
+        <div className='grid grid-rows-3 grid-cols-1 text-lg text-center mix-blend-screen md:grid md:grid-rows-2 md:grid-cols-8 justify-center mt-10 gap-4  text-orange-300 md:text-xl'>
+          <div className='hover:bg-orange-500 hover:text-black  md:col-start-3 md:col-end-5 bg-zinc-800 rounded-md shadow-xl
           outline-offset-4 flex flex-row justify-center items-center gap-2 '>
-          <a className='flex items-center gap-2' target='_blank' href=''><FaEnvelope />supunoshadha23@gmail.com</a></div>
-          <div className=' hover:bg-orange-500 hover:text-black md:col-end-7 md:col-span-2  bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2 col-end-8 col-span-3'>
-          <a className='flex items-center gap-2' ><FaPhone />(+94) 071-040-6742</a></div>
-          <div className='hover:bg-orange-500 hover:text-black md:col-start-4 md:col-end-6   bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2'><a className='flex items-center gap-2 col-start-3 col-end-6'>
-          <FaMapMarker />Wattala,Sri lanka
+          <a className='flex items-center gap-4' target='_blank' href=''><FaEnvelope />supunoshadha23@gmail.com</a></div>
+          <div className=' hover:bg-orange-500 hover:text-black md:col-end-7 md:col-span-2  bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2 col-span-3'>
+          <a className='flex items-center gap-4' ><FaPhone />(+94) 071-040-6742</a></div>
+          <div className='hover:bg-orange-500 hover:text-black md:col-start-4 md:col-end-6   bg-zinc-800 rounded-md shadow-xl outline-offset-4  items-center flex flex-row justify-center gap-2'><a className='flex items-center gap-4 '>
+          <FaMapMarker />Wattala, Sri lanka
           </a></div>
         </div>
         <div className=' flex flex-row justify-between mt-10'>              
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,10 +27,10 @@ const Hero = () => {
         </a>
     </div>
     <div className='z-20'>  
-      <h1 className='text-white text-4xl'>I'm <span className='text-orange-500 text-6xl'>Supun Oshadha</span></h1>
-      <h1 className='text-white text-4xl mt-2'>Full stack developer</h1>
-      <h1 className='text-white text-4xl mt-2'><span className='text-2xl'>founder of</span> NexNova</h1>
-      <button onClick={()=> (href="#Contact")} className='text-orange-500 border-2 rounded-lg border-orange-500 p-2 text-xl mt-8'>Contact Me</button>
+      <h1 className='text-white text-2xl md:text-4xl'>I'm <span className='text-orange-500 text-4xl md:text-6xl'>Supun Oshadha</span></h1>
+      <h1 className='text-white text-2xl md:text-4xl mt-2'>Full stack developer</h1>
+      <h1 className=' text-2xl text-orange-500 md:text-4xl mt-2'><span className='text-xl md:text-2xl text-white'>founder of</span> NexNova</h1>
+      <button onClick={()=> (href="#Contact")} className='text-orange-500 border-2 rounded-lg border-orange-500 p-2 tex-lg md:text-xl mt-8'>Contact Me</button>
       </div>
       <div className='relative '>
       <img src={Person} className='grayscale  max-h-96 rounded w-full min-h-96 min-w-72 md:max-h-screen z-10 opacity-60  mix-blend-lighten  hover:filter-none hover:animate-bounce
