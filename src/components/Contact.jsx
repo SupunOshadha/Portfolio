d0d03bc350c87d85d8e246bfa74d6971b2a0f9b0
SupunOshadha <supunoshadha23@gmail.com>
2024-10-26 16:26:56 +0530

    add-8

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,8 +14,8 @@ const Contact = () => {
     <div id='Contact' className='min-w-fit h-1/2 '>
       <div className='flex flex-row justify-between'>        
         <hr className='border-orange-400 border-b-2 w-2/5 md:border-orange-400 md:border-b-4'></hr>
-        <h1 className='text-center text-4xl -mt-5'>Contact Me</h1>           
-        <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
+        <h1 className='text-center text-xl md:text-4xl -mt-5'>Contact Me</h1>           
+        <hr className='border-b-2 border-orange-400 md:border-b-4 w-2/5 float-right '></hr>
       </div>
         <div className='text-center mix-blend-screen md:grid grid-rows-2 md:grid-cols-8 justify-center mt-10 gap-4  text-orange-300   text-xl'>
           <div className='hover:bg-orange-500 hover:text-black col-start-1 col-end-4 md:col-start-3 md:col-end-5 bg-zinc-800 rounded-md shadow-xl
@@ -28,27 +28,27 @@ const Contact = () => {
           </a></div>
         </div>
         <div className=' flex flex-row justify-between mt-10'>              
-        <hr className='border-orange-400 border-b-4 w-2/5 '></hr>    
+        <hr className='border-b-2 border-orange-400 md:border-b-4 w-2/5 '></hr>    
         <div className=' flex flex-row gap-3 justify-center z-20 -mt-3 '>
         <a target='_blank' href='https://github.com/SupunOshadha'>
-        <FaGithub className='size-8 text-white' />
+        <FaGithub className='size-5 md:size-8 text-white' />
         </a>
         <a target='_blank' href='https://www.facebook.com/profile.php?id=100010352081055&mibextid=ZbWKwL'>
-        <FaFacebook className='size-8 text-white' />
+        <FaFacebook className='size-5 md:size-8 text-white' />
         </a>
         <a target='_blank' href=' https://wa.me/qr/37WLE5PID5WPA1'>
-        <FaWhatsapp className='size-8 text-white '/>
+        <FaWhatsapp className='size-5 md:size-8 text-white '/>
         </a>
         <a target='_blank' href='https://www.linkedin.com/in/supunoshadha?'>
-        <FaLinkedinIn className='size-8 text-white '/> 
+        <FaLinkedinIn className='size-5 md:size-8 text-white '/> 
         </a>
     </div> 
-        <hr className='border-orange-400 border-b-4 w-2/5 float-right '></hr>
+        <hr className='border-b-2 border-orange-400 md:border-b-4 w-2/5 float-right '></hr>
       </div>
       <section >
       
       <footer className='mt-4'>
-        <h5 className='text-xl text-center'>All right reserved @Nexnova_Supun </h5>
+        <h5 className='text-lg md:text-xl text-center'>All right reserved @Nexnova_Supun </h5>
       </footer>    
       </section>
     </div> 
