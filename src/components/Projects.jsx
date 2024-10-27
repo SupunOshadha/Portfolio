bcb2586575ddd21034ce98794459e87df252f221
SupunOshadha <supunoshadha23@gmail.com>
2024-10-27 21:10:43 +0530

    Add-17

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FaGreaterThan } from 'react-icons/fa'
+import { FaChevronRight } from 'react-icons/fa'
+import { FaChevronLeft } from 'react-icons/fa6'
+import Beach from '../assets/images/beach.jpg'
 
 const Projects = () => {
   return (
@@ -8,13 +10,18 @@ const Projects = () => {
           <h1 className='text-center text-4xl pt-14 mb-10'>Portfolio</h1>
             <div className='m-auto flex flex-row justify-center gap-4 w-full h-screen -mt-20'>
               <div className='content-center'>
-              <button className='bg-white w-7 h-7 md:w-20 md:h-20 rounded-full mix-blend-overlay hover:mix-blend-normal '></button>
+              <button className='bg-white w-7 h-7 md:w-20 md:h-20 rounded-full mix-blend-overlay hover:mix-blend-normal '><FaChevronLeft className='m-auto bg-slate-700 size-7 '/>
+              </button>
               </div>  
               <div className='content-center w-1/2'>     
-              <div className=' h-2/3 bg-slate-500 '></div> 
+              <div className=' h-1/3 md:h-2/3 bg-slate-500 mix-blend-soft-light hover:mix-blend-normal'>
+              <a href=''>
+              <img src={Beach} className='bg-cover w-full h-full'/>
+              </a>   
+              </div> 
               </div>  
               <div className='content-center'>
-              <button className='bg-white w-7 h-7 md:w-20 md:h-20 rounded-full mix-blend-overlay hover:mix-blend-normal'><FaGreaterThan className='m-auto bg-black size-7'/></button>
+              <button className='bg-white w-7 h-7 md:w-20 md:h-20 rounded-full mix-blend-overlay hover:mix-blend-normal'><FaChevronRight className='m-auto bg-slate-700 md:size-7 '/></button>
               </div>
                           
             </div>
