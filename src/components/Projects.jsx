1decb7f275196aec896593668800a120412fd61f
SupunOshadha <supunoshadha23@gmail.com>
2024-10-27 19:50:52 +0530

    Add-14

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import { FaGreaterThan } from 'react-icons/fa'
 
 const Projects = () => {
   return (
-    <section className=' bg-zinc-700  text-white -mt-10 min-h-svh'>
+    <section className=' bg-zinc-700  text-white -mt-10 '>
         <div id='Portfolio'>
           <h1 className='text-center text-4xl pt-14 mb-10'>Portfolio</h1>
             <div className='flex flex-row justify-center gap-4 min-h-svh'>
